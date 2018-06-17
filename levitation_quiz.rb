446f9c3d44a906e8fa0d15449f6c166d811e36a6
Jesse McReady <jessemcready@gmail.com>
2018-06-17 16:38:56 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,7 +3,9 @@ def levitation_quiz
 	#your code here
 	answer = ""
 	loop do
-	  puts "What is the spell"
+	  puts "What is the spell that enacts levitation?"
+	  answer = gets.chomp
+	  break if answer == "Wingardium Leviosa"
 	end
 end
 
